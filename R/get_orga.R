d0ccd11db9ff9fdc688da968f1c1b2dc150be9a2
Malo Jan <malo.jan@sciencespo.fr>
2023-08-29 12:15:03 +0200

    Update read_csv function to read_csv2

--- R/get_orga.R
+++ R/get_orga.R
@@ -107,7 +107,7 @@ get_orga_members <- function(term, slug, include_past = FALSE) {
     }
 
     # Fetch members' data from the constructed URL
-    members_data <- readr::read_csv(url, show_col_types = FALSE)
+    members_data <- readr::read_csv2(url, show_col_types = FALSE)
 
     return(members_data)
   }
